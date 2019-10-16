446f6b48fe7a8de3e83a43923baad9be6f01d310
Joppe <joppe@apestaartje.info>
2019-10-16 07:28:30 +0200

    demos added for spring gravity and two springs

---
+++ src/demo/spring-gravity.ts
@@ -0,0 +1,127 @@
+import * as animation from '@apestaartje/animation';
+import * as dom from '@apestaartje/dom';
+import * as geometry from '@apestaartje/geometry';
+import * as physics from '@apestaartje/physics';
+
+// tslint:disable max-classes-per-file
+
+const canvas: dom.element.Canvas = new dom.element.Canvas({
+    width: 800,
+    height: 800,
+});
+
+canvas.appendTo(document.body);
+
+class Anchor {
+    private readonly _diameter: number = 5;
+    private readonly _position: geometry.vector.Vector;
+
+    constructor(position: geometry.vector.Vector) {
+        this._position = position;
+    }
+
+    public render(context: CanvasRenderingContext2D): void {
+
+        context.fillStyle = '#0000ff';
+        context.beginPath();
+        context.arc(
+            this._position.x,
+            this._position.y,
+            this._diameter,
+            0,
+            Math.PI * 2,
+            true,
+        );
+        context.closePath();
+        context.fill();
+    }
+}
+
+class Ball {
+    private readonly _diameter: number = 20;
+
+    public render(context: CanvasRenderingContext2D, position: geometry.vector.Vector): void {
+        context.fillStyle = '#00ff00';
+        context.beginPath();
+        context.arc(
+            position.x,
+            position.y,
+            this._diameter,
+            0,
+            Math.PI * 2,
+            true,
+        );
+        context.closePath();
+        context.fill();
+    }
+}
+
+class Line {
+    public render(context: CanvasRenderingContext2D, start: geometry.vector.Vector, end: geometry.vector.Vector): void {
+        context.strokeStyle = '#ff0000';
+        context.beginPath();
+        context.moveTo(start.x, start.y);
+        context.lineTo(end.x, end.y);
+        context.closePath();
+        context.stroke();
+    }
+}
+
+interface Stage {
+    render(context: CanvasRenderingContext2D, dt: number): void;
+}
+
+const stage: Stage = ((): Stage => {
+    const mass: number = 1;
+    const ball: Ball = new Ball();
+    const center: geometry.vector.Vector = {
+        x: 350,
+        y: 50,
+    };
+    let position: geometry.vector.Vector = {
+        x: 350,
+        y: 50,
+    };
+    const anchor: Anchor = new Anchor(center);
+
+    const c: number = 0.5; // damping constant
+    const k: number = 10;  // restoring constant
+
+    let velocity: geometry.vector.Vector = { x: 0, y: 0 };
+    let acceleration: geometry.vector.Vector = { x: 0, y: 0 };
+
+    const button: HTMLButtonElement = document.createElement('button');
+    button.innerText = 'Move';
+    button.addEventListener('click', (): void => {
+        velocity.x += 200;
+    });
+    document.body.appendChild(button);
+
+    return {
+        render(context: CanvasRenderingContext2D, dt: number): void {
+            position = physics.move.position(position, velocity, dt);
+
+            anchor.render(context);
+            ball.render(context, position);
+
+            const displacement: geometry.vector.Vector = geometry.vector.subtract(position, center);
+            const restoring: geometry.vector.Vector = physics.force.spring.restoring(k, displacement);
+            const damping: geometry.vector.Vector = physics.force.spring.damping(c, velocity);
+            const force: geometry.vector.Vector = physics.force.add(restoring, damping);
+
+            acceleration = physics.move.acceleration(force, mass);
+            velocity = physics.move.velocity(velocity, acceleration, dt);
+        },
+    };
+})();
+
+const animator: animation.animator.Animator = new animation.animator.Animator((time: animation.animator.Chronometer): boolean => {
+    const dt: number = time.offset * 0.001;
+
+    canvas.clear();
+    stage.render(canvas.context, dt);
+
+    return true;
+});
+
+animator.start();
